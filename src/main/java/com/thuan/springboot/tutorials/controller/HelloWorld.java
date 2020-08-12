62f485acd22cb89a51a783deb1b02a7f8a456ebf
Thuan <thuan.uit@gmail.com>
2020-08-12 15:17:02 +0700

    First commit

---
+++ src/main/java/com/thuan/springboot/tutorials/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.thuan.springboot.tutorials.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloWorld {
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello World Developer!!!";
+	}
+}
